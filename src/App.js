fe3181930fd23293a23dfe1a58b1862aa40df5ee
dqarias <dino.quispea@gmail.com>
2022-07-26 10:41:50 -0500

    Fetch data from rest countries API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import fetchCountries from './Services/restCountriesAPI';
 
 function App() {
+  const america = 'Europe';
+
+  const getCountry = async () => {
+    const getRegion = await fetchCountries(america);
+    console.log(getRegion);
+  };
+
+  getCountry();
   return (
     <div className="App">
       Hello React
