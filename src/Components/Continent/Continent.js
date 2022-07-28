9007c3aac180cd6868ddd6802fcc8d398aced4e1
dqarias <dino.quispea@gmail.com>
2022-07-28 01:15:32 -0500

    Add an country and continent's map

--- src/Components/Continent/Continent.js
+++ src/Components/Continent/Continent.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import URL_IMAGE from '../../Utils/mapImage';
 
 const Continent = ({ name }) => {
   const navigate = useNavigate();
   const handleCountries = () => {
     navigate(`/${name}`);
   };
+  const continentName = name.toLowerCase();
 
   return (
     <li
       onClick={() => handleCountries()}
       aria-hidden="true"
     >
-      {name}
+      
+        <img src={continentName === 'americas' ? `${URL_IMAGE}/america/vector.svg` : `${URL_IMAGE}/${continentName}/vector.svg` } alt="" />
+      <p>{name}</p>
     </li>
   );
 };
--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import URL_IMAGE from '../../Utils/mapImage';
 
 const Country = ({
   name, population, area, latitude, longitude, region, altSpelling,
@@ -8,11 +9,14 @@ const Country = ({
   const handleAirPollution = () => {
     navigate(`/${name}/${altSpelling}/${latitude}/${longitude}`);
   };
+
+  const imageCountry = altSpelling.toLowerCase();
   return (
     <li
       onClick={() => handleAirPollution()}
       aria-hidden="true"
     >
+      <img src={`${URL_IMAGE}/${imageCountry}/vector.svg` } alt="" />
       <p>{name}</p>
       <p>{population}</p>
       <p>{area}</p>
